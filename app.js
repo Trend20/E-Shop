f25fda0cc039e231cbd75bc69739b05a4baef418
devenock <enockomondi305@gmail.com>
2020-11-26 02:14:45 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ejs = require('ejs');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+
+// initialize eprexx app
+const app = express();
+
+// use middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// setting the view engine
+app.set('view engine', 'ejs');
+
+// adding static files
+app.use(express.static(__dirname + '/public'));
+
+// port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>{
+    console.log(`Application running on port ${PORT}`);
+});
